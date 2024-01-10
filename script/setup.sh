09de2fe7165dda9eaa4c2a8e163d866feb715b13
khangkontum <khangkontum@gmail.com>
2024-01-10 13:28:57 +0700

    fix: typo

--- script/setup.sh
+++ script/setup.sh
@@ -9,7 +9,7 @@ echo "Install python"
 sudo apt-get install libssl-dev openssl
 wget https://www.python.org/ftp/python/$PYTHON_VERSION/Python-$PYTHON_VERSION.tgz
 tar xzvf Python-$PYTHON_VERSION.tgz
-cd $PYTHON_VERSION || echo "Failed to install python"
+cd Python-$PYTHON_VERSION || echo "Failed to install python"
 ./configure
 make
 sudo make install
