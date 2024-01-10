e08b7601c81ccbc0cfb1638228dc5419b25fb34a
khangkontum <khangkontum@gmail.com>
2024-01-10 13:24:40 +0700

    fix: typo

--- script/setup.sh
+++ script/setup.sh
@@ -3,14 +3,13 @@
 
 export PYTHON_VERSION="3.11.5"
 
-apt-get update
-apt-get install build-essential
+sudo DEBIAN_FRONTEND=noninteractive apt-get update && apt-get install -y build-essential
 
 echo "Install python"
 sudo apt-get install libssl-dev openssl
-wget https://www.python.org/ftp/python/"$(PYTHON_VERSION)"/"$(PYTHON_VERSION)".tgz
-tar xzvf "$(PYTHON_VERSION)".tgz
-cd "$(PYTHON_VERSION)" || echo "Failed to install python"
+wget https://www.python.org/ftp/python/$PYTHON_VERSION/Python-$PYTHON_VERSION.tgz
+tar xzvf Python-$PYTHON_VERSION.tgz
+cd $PYTHON_VERSION || echo "Failed to install python"
 ./configure
 make
 sudo make install
